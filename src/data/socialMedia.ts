f1c05b44f45d35e7c5f6235c017ca22050ccfd69
Ayyoubkhelifi <abderrahmenedz12@gmail.com>
2025-09-27 17:05:56 +0100

    Social Media Links

--- src/data/socialMedia.ts
+++ src/data/socialMedia.ts
@@ -2,13 +2,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/Ayyoubkhelifi",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/ayyoub-khelifi-17a79219a",
   },
 ];
