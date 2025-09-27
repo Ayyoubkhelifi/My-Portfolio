b16f8be7954317ecb1fed65e7014f31c08da4d97
Ayyoubkhelifi <abderrahmenedz12@gmail.com>
2025-09-27 16:47:35 +0100

    Added Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import RecentProjects from "./components/home/RecentProjects";
 import Clients from "./components/home/Clients";
 import Experience from "./components/home/Experience";
 import Approach from "./components/home/Approach";
+import Footer from "./components/home/Footer";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
